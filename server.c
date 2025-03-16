1e8c6c46b4e9c25d2c5e8c551ea4f10150a94bcd
moumniyoussef <youssef18moumni.com>
2025-03-16 16:10:11 +0000

    done

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: youmoumn <youmoumn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/16 15:11:29 by youmoumn          #+#    #+#             */
-/*   Updated: 2025/03/16 16:07:28 by youmoumn         ###   ########.fr       */
+/*   Updated: 2025/03/16 16:09:44 by youmoumn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,8 @@ int main(int ac, char **av)
 	int PID = getpid();
 	printf("PID : [%d]\n", PID);
 	// kill(PID, SIGKILL);
-	pause();
+	while(1)
+	{
+		pause();
+	}
 }
